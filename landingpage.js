6bf7f0459d3d7c87072a9f784bc4b69fffd4bfa8
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2023-03-21 17:48:59 +0100

    whole website

---
+++ landingpage.js
@@ -0,0 +1,9 @@
+function init(){
+    setTimeout('document.location="Onas.html"', 0.001);
+}
+function init2(){
+    setTimeout('document.location="zjecia.html"', 0.001);
+}
+function init3(){
+    setTimeout('document.location="koszyk.html"', 0.001);
+}
